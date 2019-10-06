f115a6904c309875554d2456d9ee7c70e81056c8
prehooo <kamilnatonek@gmail.com>
2019-10-06 18:30:54 +0200

    update button added, images, progresshud update

--- Api/MyTableViewController.swift
+++ Api/MyTableViewController.swift
@@ -13,32 +13,20 @@ import SVProgressHUD
 
 class MyTableViewController: UITableViewController {
    
-
-    var  val1 : Double = 0
-//    var val2 : String = ""
-    var priceArray = [Double]()
-    let api_key = "7c20af7a59852d33c46c5d760fdc954e"
-    let url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,XRP,BCH,EOS&tsyms=PLN"
+    @IBOutlet var reloadButton: UIButton!
+    
+        let cryptoImageArray = ["btc","eth","ltc","xrp","eos"]
+        var  val1 : Double = 0
+        var priceArray = [Double]()
+        let api_key = "7c20af7a59852d33c46c5d760fdc954e"
+        let url = "https://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,XRP,BCH,EOS&tsyms=PLN"
     override func viewDidLoad() {
+        reloadButton.isHidden = true
         super.viewDidLoad()
+        request()
 //        Alamofire.request(url).response { response in
 //            debugPrint(response)
-    SVProgressHUD.show()
-            Alamofire.request(url, method: .get)
-                        .responseJSON { response in
-                            if response.result.isSuccess {
-
-                                print("Sucess!")
-                                
-                                let priceJSON: JSON = JSON(response.result.value!)
-                                
-                                print(priceJSON)
-                         self.parseJson(json: priceJSON)
-                            } else {
-                                print("Error: \(String(describing: response.result.error))")
-                              
-                            }
-        }
+   
     }
     
     func parseJson(json : JSON) {
@@ -47,7 +35,7 @@ class MyTableViewController: UITableViewController {
         priceArray.append(json["ETH"]["PLN"].doubleValue) // 2 ETH
         priceArray.append(json["LTC"]["PLN"].doubleValue) // 3 LTC
         priceArray.append(json["XRP"]["PLN"].doubleValue)// 4 XRP
-        priceArray.append(json["BCH"]["PLN"].doubleValue)// 5 BITCOIN CASH
+//        priceArray.append(json["BCH"]["PLN"].doubleValue)// 5 BITCOIN CASH
         priceArray.append(json["EOS"]["PLN"].doubleValue) // 6 EOS
         
 //        val1 = json["id"].intValue
@@ -58,7 +46,9 @@ class MyTableViewController: UITableViewController {
 //        let val2 = json["weather"][0]["id"].intValue
      print(priceArray)
         tableView.reloadData()
-       SVProgressHUD.dismiss()
+       SVProgressHUD.dismiss(withDelay: 1)
+        reloadButton.isHidden = false
+        
     }
     
 
@@ -76,8 +66,34 @@ class MyTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
         
         cell.textLabel?.text = String(priceArray[indexPath.row])
+        cell.imageView?.image = UIImage(named: cryptoImageArray[indexPath.row])
+        
         return cell
+        
+    }
+    func request() {
+        SVProgressHUD.show()
+        Alamofire.request(url, method: .get)
+            .responseJSON { response in
+                if response.result.isSuccess {
+                    
+                    print("Sucess!")
+                    SVProgressHUD.showSuccess(withStatus: "Sucess!")
+                    let priceJSON: JSON = JSON(response.result.value!)
+                    
+                    print(priceJSON)
+                    self.parseJson(json: priceJSON)
+                } else {
+                    
+                    print("Error: \(String(describing: response.result.error))")
+                    
+                }
+        }
     }
     
+    @IBAction func ReloadData(_ sender: Any) {
+        priceArray.removeAll()
+        request()
+    }
 }
 
